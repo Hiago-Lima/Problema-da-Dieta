f3aada1931cc2d55ae1d0fdff259d42da59f6968
Hiago Lima <hiago.olima1@gmail.com>
2025-11-05 20:43:53 -0300

    so apelidei o linprog

--- Problema_da_dieta.py
+++ Problema_da_dieta.py
@@ -2,7 +2,7 @@
 import pandas as pd # mexer com base de dados(nesse caso, tabela)
 import numpy as np # fazer cálculos numéricos
 import matplotlib.pyplot as plt #plotar gráficos, nao sei se a gnt vai usar
-from scipy.optimize import linprog # minimizar ou maximizar funções
+from scipy.optimize import linprog as simplex # minimizar ou maximizar funções
 
 df = pd.read_excel("tabela_dieta.xlsx") # colocando a tabela em uma variável pra facilitar nossa vida
 print("=== Dados dos Alimentos ===")
