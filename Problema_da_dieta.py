d2f47198903c208d96888a70c5f9a1a80be32611
Hiago Lima <hiago.olima1@gmail.com>
2025-11-16 19:51:43 -0300

    comentarios e mudança no excalidraw

--- Problema_da_dieta.py
+++ Problema_da_dieta.py
@@ -18,22 +18,29 @@ b = np.array([1870, 46, 1.0, 1.5, 0.018, 0.775])
 A_ub = -A
 b_ub = -b
 
-# Limites das variáveis (x >= 0)
+# Limites das variáveis (x >= 0) * 13( quantidade de alimentos) minímas
 bounds = [(0, None)] * len(df)
-# Resolver o modelo
+# Resolver o modelo utilizando o simplex()
+# passa todos os parametros para resolver e utiliza o método highs
 res = simplex(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds, method='highs')
 
 # Mostrar resultados
 if res.success:
+    # contém o valor de cada variável x
     df['Porcao_ideal'] = res.x
+    # Multiplica cada porção pelo preço correspondente
     df['Custo_total_R$'] = df['Preço (por porção)'] * df['Porcao_ideal']
 
     print("=== SOLUÇÃO ENCONTRADA ===")
     print(df[['Alimentos', 'Porcao_ideal', 'Custo_total_R$']])
+    #res.fun é o valor mínimo encontrado da função de custo.
+    #{:.2f} formata com 2 casas decimais.
     print("\nCusto mínimo total = R$ {:.2f}".format(res.fun))
 
     # Nutrientes atingidos (A x)
+    # Multiplica a matriz de nutrientes pela quantidade de porções escolhidas.
     nutrientes_totais = A.dot(res.x)
+    #Criando uma tabela comparando atingido vs necessário:
     df_nut = pd.DataFrame({
         'Nutriente': ['Energia (kcal)', 'Proteína (g)', 'Cálcio (g)', 'Sódio (g)', 'Ferro (g)', 'Vitaminas (g)'],
         'Total_obtido': nutrientes_totais,
